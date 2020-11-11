2dd6a035c822c99af237ed3d0b112e6a210753c9
sofia <sbozinos@htl-steyr.ac.at>
2020-11-11 11:32:19 +0100

    Änderung: Ausgabe "Hallo Chantal & Leonita"

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,7 @@ public class Main {
     public static void main(String[] args){
 
         System.out.println("hallo sofia");
+        System.out.println("Hallo Leonita -S");
+        System.out.println("Hallo Chantal -S");
     }
 }
